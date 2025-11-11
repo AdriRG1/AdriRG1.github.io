157c1a933e05b69bb928d6c6d978a756c9728681
AdriRG1 <adrinegales@gmail.com>
2025-11-11 12:16:23 +0100

    Add files via upload

---
+++ ejercicios_p5js/rodriguez_adrian_p5_1bis.js
@@ -0,0 +1,34 @@
+function setup() {
+  createCanvas(800, 800);
+  background(0, 128, 0);
+  strokeWeight(8);
+
+  stroke(random(255), random(255), random(255));
+  line(100, 600, 150, 400);
+  stroke(random(255), random(255), random(255));
+  line(150, 400, 200, 600);
+  stroke(random(255), random(255), random(255));
+  line(120, 500, 180, 500);
+
+  stroke(random(255), random(255), random(255));
+  line(250, 400, 250, 600);
+  stroke(random(255), random(255), random(255));
+  line(250, 400, 320, 430);
+  stroke(random(255), random(255), random(255));
+  line(320, 430, 320, 570);
+  stroke(random(255), random(255), random(255));
+  line(320, 570, 250, 600);
+
+
+  stroke(random(255), random(255), random(255));
+  line(380, 400, 380, 600);
+  stroke(random(255), random(255), random(255));
+  line(380, 400, 440, 430);
+  stroke(random(255), random(255), random(255));
+  line(440, 430, 380, 470);
+  stroke(random(255), random(255), random(255));
+  line(380, 470, 440, 600);
+
+  stroke(random(255), random(255), random(255));
+  line(500, 400, 500, 600);
+}
